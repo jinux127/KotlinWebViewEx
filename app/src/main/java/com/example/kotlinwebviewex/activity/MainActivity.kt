3b17497542e420d919f7e1e1e2507f6853b4fb40
jinwoo <wwo1278@gmail.com>
2021-06-03 16:51:00 +0900

    웹뷰 alert, javascript호출

--- app/src/main/java/com/example/kotlinwebviewex/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlinwebviewex/activity/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.kotlinwebviewex.activity
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinwebviewex.R
 
 class MainActivity : AppCompatActivity() {
@@ -36,10 +37,21 @@ class MainActivity : AppCompatActivity() {
         mWebView.addJavascriptInterface(WebAppInterface(this),"Android") //Javascript 설정
         mWebView.webViewClient = WebViewClientClass()
 
+        /*
+        * WebView에서 javascript를 사용하도록 설정하더라도 alert은 동작하지 않는다.
+        * 브라우저에서 지원하는 API이기 때문이다.
+        * WebChromeClient에는 alert 발생시 동작하는 onJSAlert가 있다.
+        * 혹시 alert를 다른 방식으로 표현해주기 위해서는 onJsAlert내부에 표현방식을 구현한 후 return 값만 true로 바꿔주면 된다.
+        * 이를 이용하기 위해 설정
+        * */
+        mWebView.webChromeClient = WebChromeClient()
+
     }
+
     class WebAppInterface(private val mContext:Context){
         @JavascriptInterface
         fun showToast(toast:String){
+            Log.d("111","눌림")
             Toast.makeText(mContext,toast, Toast.LENGTH_SHORT).show()
         }
     }
